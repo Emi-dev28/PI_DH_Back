872f6082f6474948be81cc3a7f05bc0f16c7459d
emi-dev28 <emilianocellilli00@gmail.com>
2023-10-20 12:08:47 -0300

    Set-up inicial, directorios y dependencias

---
+++ src/main/java/com/PI_back/pi_back/PiBackApplication.java
@@ -0,0 +1,13 @@
+package com.PI_back.pi_back;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PiBackApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PiBackApplication.class, args);
+	}
+
+}
