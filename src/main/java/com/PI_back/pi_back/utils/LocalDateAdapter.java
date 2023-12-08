fc3fb82489aa0f864a4f751220aa57bf6c429503
Emiliano <emilianocellilli00@gmail.com>
2023-12-08 10:07:05 -0300

    Create Adapters
    - Manages the dates

---
+++ src/main/java/com/PI_back/pi_back/utils/LocalDateAdapter.java
@@ -0,0 +1,24 @@
+package com.PI_back.pi_back.utils;
+import com.fasterxml.jackson.core.Version;
+import com.fasterxml.jackson.databind.Module;
+import com.google.gson.*;
+import java.lang.reflect.Type;
+import java.time.LocalDate;
+import java.time.format.DateTimeFormatter;
+import java.util.Locale;
+
+public class LocalDateAdapter  implements JsonSerializer<LocalDate>, JsonDeserializer<LocalDate> {
+
+    private static final DateTimeFormatter dateFormatter = DateTimeFormatter.ofPattern("yyyy-MM-dd");
+
+    @Override
+    public LocalDate deserialize(JsonElement jsonElement, Type type, JsonDeserializationContext jsonDeserializationContext) throws JsonParseException {
+        return LocalDate.parse(jsonElement.getAsString(),
+                DateTimeFormatter.ofPattern("yyyy-MM-dd").withLocale(Locale.US));
+    }
+
+    @Override
+    public JsonElement serialize(LocalDate localDate, Type type, JsonSerializationContext jsonSerializationContext) {
+        return new JsonPrimitive(dateFormatter.format(localDate));
+    }
+}
