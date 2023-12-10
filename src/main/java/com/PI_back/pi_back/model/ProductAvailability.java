8b6e7ccafb8f1158d88607631104a506040bd123
Emiliano <emilianocellilli00@gmail.com>
2023-12-10 19:52:59 -0300

    Add jackson notations

--- src/main/java/com/PI_back/pi_back/model/ProductAvailability.java
+++ src/main/java/com/PI_back/pi_back/model/ProductAvailability.java
@@ -1,7 +1,6 @@
 package com.PI_back.pi_back.model;
 
-import com.fasterxml.jackson.annotation.JsonFormat;
-import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
+import com.fasterxml.jackson.annotation.*;
 import jakarta.persistence.*;
 import lombok.*;
 import org.hibernate.annotations.DynamicInsert;
@@ -10,6 +9,7 @@ import org.hibernate.annotations.DynamicUpdate;
 import java.text.SimpleDateFormat;
 import java.time.LocalDate;
 import java.time.LocalDateTime;
+import java.util.Set;
 
 
 @NoArgsConstructor
@@ -25,12 +25,19 @@ import java.time.LocalDateTime;
 public class ProductAvailability {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @JsonIgnore
     private Long id;
     @Column(name = "FromDate")
     @JsonFormat(shape = JsonFormat.Shape.STRING, pattern = "yyyy-MM-dd")
+    @JsonProperty(value = "fromDate")
     private LocalDate fromDate;
     @Column(name = "ToDate")
+    @JsonProperty(value = "toDate")
     @JsonFormat(shape = JsonFormat.Shape.STRING, pattern = "yyyy-MM-dd")
     private LocalDate toDate;
+    @ManyToOne(fetch = FetchType.LAZY, cascade = CascadeType.ALL)
+    @JoinColumn(name = "product_id", nullable = false)
+    @JsonBackReference
+    private Product product;
 
 }
